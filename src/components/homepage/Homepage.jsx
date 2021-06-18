ac6e1eae26b11e8c03acd61dd828a73efd544e84
Vasily <ckpyt@inbox.ru>
2021-06-18 19:23:47 +0800

    add material templates, add basic layots, scss

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -3,18 +3,26 @@ import React from 'react'
 import Menulist from '../menulist/Menulist'
 import Products from '../products/Products'
 import Navbar from '../Navbar/Navbar'
+import { Paper } from '@material-ui/core'
+
 
 export default function Homepage() {
     return (
         <div className="homepage-container">
-            <div className="nav-bar-container">
-            <Navbar/>
-            </div>
-            
-            <div className="menu-bar-desktop">
+            <Navbar />
+            <div className="homepage-grid">
                 <Menulist />
+                <div className="products-wrap">
+                    <div className="products-top-wrap">
+                        <div className="products-banner"><Paper><Products /></Paper></div>
+                        <div className="products-actions"><Paper>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, ex qui consectetur error reprehenderit numquam omnis quos quis voluptates repudiandae tenetur aliquam similique voluptas recusandae officiis non delectus quas labore?</Paper></div>
+                    </div>
+                    <div className="menu-mobile"><Paper>menu-mobilemenu-mobilemenu-mobilemenu-mobilemenu-mobilemenu-mobilemenu-mobilemenu-mobilemenu-mobilemenu-mobilemenu-mobile</Paper></div>
+                    <div className="products-hot"><Paper>products-hotproducts-hotproducts-hotproducts-hotproducts-hotproducts-hotproducts-hotproducts-hotproducts-hotproducts-hotproducts-hotproducts-hot</Paper></div>
+
+                </div>
             </div>
-            <Products />
+
         </div>
     )
 }
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function Products() {
     return (
-        <div>
-            alksd
+        <div className="product">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium dolores nihil quam magnam distinctio sequi, fuga officiis placeat quidem.
         </div>
     )
 }
